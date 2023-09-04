d79198dd43915dd9d7d3738e0a6103785e533f2d
Prateek Krishna <prateekkrishna09@gmail.com>
2023-09-05 01:37:25 +0530

    added bg canvas

---
+++ src/pages/ShootingStars.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ShootingStars = () => {
+  return (
+    <section>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        
+    </section>
+  )
+}
+
+export default ShootingStars
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Landing from './Landing'
+import Animation from './Animation'
+import ShootingStars from './ShootingStars'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div >
-      <Landing />
+      {/* <Landing /> */}
+      {/* <ShootingStars /> */}
+      <Animation />
+
     </div>
   )
 }
