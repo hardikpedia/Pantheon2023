2ddb06d076fc69d7dd96188f34564affaeeadf0d
Prateek Krishna <prateekkrishna09@gmail.com>
2023-09-09 00:21:10 +0530

    counter color changed

--- src/components/TimerComponent.js
+++ src/components/TimerComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 const TimerComponent = ({ label, value }) => {
   return (
     <div className="text-center mx-1 md:mx-2 my-1 md:my-4">
-      <h1 className="text-3xl font-bold sm:text-6xl z-100 text-pink-600">{value}</h1>
+      <h1 className="text-3xl font-bold sm:text-6xl z-100 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">{value}</h1>
       <h2 className=" text-[8px] sm:text-sm sm:-mt-9 -mt-[22px] text-white -z-40 mb-4 ">{label}</h2>
     </div>
   )
