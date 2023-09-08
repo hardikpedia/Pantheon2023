fe7b4d7fdc825774f2b57346d4671d82db55966b
Prateek Krishna <prateekkrishna09@gmail.com>
2023-09-08 23:37:39 +0530

    minor changes

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import Image from "next/image"
 
 const events = () => {
   return (
-    <div className='bg-black text-white h-screen flex justify-center items-center' >
-        
+    <div className= 'bg-black text-white h-screen flex items-center justify-center'>
+        <div className='card' >
+            <Image src='/pics.jpeg' alt='poster' height="500" width="320" 
+             className="card-img"
+            />
+        </div>
     </div>
   )
 }
