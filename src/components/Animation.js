a455a975ea0ede630de36ae2f835f3cc3cf4c804
legendary0911 <85240150+legendary0911@users.noreply.github.com>
2023-09-14 01:57:47 +0530

    speed updated

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -39,7 +39,7 @@ export default function Animation() {
           minDistance={5}
           maxDistance={50}
           autoRotate={true}
-          autoRotateSpeed={0.3}
+          autoRotateSpeed={0.5}
         />
         <EffectComposer>
           <Bloom mipmapBlur luminanceThreshold={1} />
