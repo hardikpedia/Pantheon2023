b19d63e78ffcde1aa0cf145047ee4481af58083b
Prateek Krishna <prateekkrishna09@gmail.com>
2023-09-14 18:47:29 +0530

    minor changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,12 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Landing from '../components/Landing'
 import Animation from '../components/Animation'
-import Navbar from '../components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
-      {/* <Navbar /> */}
       <div className=''>
         <Animation />
 
