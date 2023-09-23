1a15b1b4ea878f7b6428da13e1ef46b8dc41cb2e
12Shantanu <shantanu.sameer12@gmail.com>
2023-09-23 21:28:04 +0530

    initial commit

---
+++ src/components/Slider.js
@@ -0,0 +1,84 @@
+import React, {Component} from "react";
+import styles from "../styles/styles";
+import { useSelector } from "react-redux";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Color } from "three";
+
+
+
+const Sliderss = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear"
+  };
+  return (
+    <>
+    
+    <div
+      className={`bg-black ${styles.section} hidden sm:block bg-white py-10 px-5 mb-12 cursor-pointer rounded-xl`}
+    >
+
+<div >
+        
+        <Slider {...settings}>
+        <div className="container">
+          <img
+            src="https://logos-world.net/wp-content/uploads/2020/04/Sony-Logo.png"
+            alt=""
+            className="bg-white"
+            style={{width:"150px", objectFit:"contain"}}
+          />
+          </div>
+
+
+
+          <div className="container">
+          <img
+            src="https://www.vectorlogo.zone/logos/apple/apple-ar21.png"
+            style={{width:"150px", objectFit:"contain"}}
+            alt=""
+          />
+          </div>
+
+          <div className="container">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/LG_logo_%282015%29.svg/2560px-LG_logo_%282015%29.svg.png"
+            style={{width:"150px", objectFit:"contain"}}
+            alt=""
+          />
+          </div>
+
+          <div className="container">
+          <img
+            src="https://logos-world.net/wp-content/uploads/2020/08/Dell-Logo-1989-2016.png"
+            style={{width:"150px", objectFit:"contain"}}
+            alt=""
+          />
+          </div>
+          <div className="container">
+          <img
+            src="https://logos-world.net/wp-content/uploads/2020/04/Sony-Logo.png"
+            alt=""
+            style={{width:"150px", objectFit:"contain"}}
+          />
+          </div>
+        </Slider>
+
+
+      </div>    
+          </div>
+         
+    </>
+  );
+};
+
+export default Sliderss;
---
+++ src/pages/sponsors.js
@@ -0,0 +1,145 @@
+import React from "react";
+// import Header from "../components/Header";
+// import iibg from "../assets/bg/bg.jpg";
+// import iiibg from "../assets/bg/bg.jpg";
+
+import Sliderss from "../components/Slider";
+import SponsorBox from "../pages/Sponsors/Sponsor";
+import styled from "styled-components";
+import {
+  NewSponsors,
+  TitleSponsor,
+  copreSponsor,
+  associationSponsor,
+  investSponsor,
+  mediaSponsor,
+  gameSponsor,
+  learnSponosor,
+  startSponsor,
+  copowSponsor,
+} from "./data";
+import { sponsors,TitleSponsor1,
+  copreSponsor1,
+  associationSponsor1,mediaSponsor1,learnSponosor1,
+  startSponsor1,
+  copowSponsor1, } from "../utils/Sponsors23";
+
+
+const Sponsors = ({ data }) => {
+  return (
+    <>
+      <div className="  overflow-hidden  ">
+        <div
+          style={{ backgroundImage: ` url(${`images/bg.jpg`})` }}
+          className=" h-full w-[100%] bg-cover bg-black"
+        >
+          <div className="pt-8">
+            <SectionContainer id="sponsors">
+              <Heading className="text-white">Our Sponsors</Heading>
+              <SponsorHeading>Title Sponsor</SponsorHeading>
+             
+             
+              <h1 className="p-4"> 
+
+</h1>
+            
+              <EventsContainer>
+                {learnSponosor1.map((sponsor) => {
+                  return <SponsorBox data={sponsor} />;
+                })}
+              </EventsContainer>
+              
+              <SponsorHeading>
+                <h1 className="p-4"> 
+
+                </h1>
+                Past Sponsor
+                <h1 className="p-4"> 
+
+                </h1>
+                </SponsorHeading>
+             
+             
+             
+             <Sliderss />
+            </SectionContainer>
+           
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sponsors;
+
+const SectionContainer = styled.div`
+  padding: 50px 0px 100px 0px;
+  @media screen and (max-width: 800px) {
+    padding: 50px 0px 50px 0px;
+    text-align: left;
+    border-bottom: 0.2px solid rgba(255, 255, 255, 0.5);
+  }
+  text-align: center;
+  font-weight: 900;
+`;
+
+const Heading = styled.h1`
+  display: block;
+  background-image: linear-gradient(
+    to right,
+    #ffc100,
+    #ffc100,
+    #ffc100,
+    #ffbf00,
+    #ffbf00
+  );
+  -webkit-background-clip: text;
+  background-clip: text;
+  
+  font-size: 56px;
+  padding-bottom: 50px;
+  text-align: center;
+  font-family: Orbitron;
+  font-weight: 900;
+`;
+
+const SponsorHeading = styled.h1`
+  display: block;
+  background-image: linear-gradient(
+    to right,
+    #ffc107,
+    #ffc107,
+    #ffc107,
+    #ffc107,
+    #ffc107
+  );
+  -webkit-background-clip: text;
+  background-clip: text;
+  
+  color: white;
+  font-size: 24px;
+  text-align: center;
+`;
+//background-image: linear-gradient(to right, #f76c6c, #e8637c, #d45f8a, #bc5e93, #a25e97);
+
+const EventsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  border-radius: 20px;
+  padding: ${(props) => {
+    if (props.back) {
+      return "20px";
+    } else {
+      return "20px 0px 30px 0px";
+    }
+  }};
+  background-color: ${(props) => {
+    if (props.back) {
+      return "#151629";
+    } else {
+      return "rgba(0,0,0,0)";
+    }
+  }};
+`;
